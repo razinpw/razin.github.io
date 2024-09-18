95bc4d41b1a195ebfcb534481eb0036e1a8d7550
razinpw <razindev@internet.ru>
2024-09-18 20:45:41 +0300

    inital commit

---
+++ js/config.js
@@ -0,0 +1,44 @@
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 1,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 1.5, 
+
+  // Заголовок
+  title: 'however',
+
+  // Социальные сети
+  social: {
+    telegram_kroysi: '@kroysi',
+    vk_kroysi: 'kroysi',
+  }, 
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#1E90FF',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#00BFFF',
+  // Цвет тени кнопки play
+  playButtonShadowColor: 'rgba(255, 255, 255, 0.5)',
+
+
+  // Цвет заголовка
+  titleColor: '#00BFFF',
+  // Цвет тени загаловка
+  titleShadowColor: 'rgba(255, 255, 255, 0.5)',
+
+
+  // Текст копирайта
+  copyrightText: 'i am god',
+  // Цвет копирайта
+  copyrightTextColor: '#0000CD',
+
+  // Цвет иконок социальных сетей
+  socialColor: '#00BFFF',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: 'rgba(255, 255, 255, 0.5)',
+}
